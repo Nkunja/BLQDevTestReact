3d41784b5bb1114e1f187d8de46ad776b51229a3
Nkunja~ <munene01denis@gmail.com>
2024-02-04 09:16:02 +0300

    Changes Swipe Screen

--- src/features/home/components/ImageCarousel.tsx
+++ src/features/home/components/ImageCarousel.tsx
@@ -16,8 +16,8 @@ export default function MainBanner() {
 
   return (
     <Swiper
-    slidesPerView={2}
-    spaceBetween={30}
+      slidesPerView={2}
+      spaceBetween={30}
       loop={true}
       freeMode={true}
       centerInsufficientSlides
@@ -29,15 +29,16 @@ export default function MainBanner() {
         prevEl: ".swiper-button-prev",
       }}
       modules={[Navigation]}
+      autoplay={{ delay: 5000 }} // Adjust delay as needed
       className="mySwiper"
-      style={{ width: "100%", height:"250px" }} 
+      style={{ width: "100%", height: "250px" }}
     >
       {mainBanner.map((banner, index) => (
         <SwiperSlide
           key={banner.mainBannerId}
           style={{
-            width: index === 1 ? "80%" : "0%", 
-            margin: "auto", 
+            width: index === 1 ? "80%" : "0%",
+            margin: "auto",
           }}
         >
           <img
@@ -52,3 +53,59 @@ export default function MainBanner() {
     </Swiper>
   );
 }
+
+
+// import { useEffect } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import { Navigation } from "swiper/modules";
+// import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+// import { getMainBannerAsync } from "../homeAsync";
+
+// export default function MainBanner() {
+//   const dispatch = useAppDispatch();
+//   const { mainBanner } = useAppSelector((state) => state.home);
+
+//   useEffect(() => {
+//     dispatch(getMainBannerAsync());
+//   }, [dispatch]);
+
+//   return (
+//     <Swiper
+//     slidesPerView={2}
+//     spaceBetween={30}
+//       loop={true}
+//       freeMode={true}
+//       centerInsufficientSlides
+//       pagination={{
+//         clickable: true,
+//       }}
+//       navigation={{
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       }}
+//       modules={[Navigation]}
+//       className="mySwiper"
+//       style={{ width: "100%", height:"250px" }} 
+//     >
+//       {mainBanner.map((banner, index) => (
+//         <SwiperSlide
+//           key={banner.mainBannerId}
+//           style={{
+//             width: index === 1 ? "80%" : "0%", 
+//             margin: "auto", 
+//           }}
+//         >
+//           <img
+//             src={banner.pcImageUrl}
+//             alt={banner.title}
+//             className="h-full w-full object-cover"
+//           />
+//         </SwiperSlide>
+//       ))}
+//       <div className="swiper-button-prev"></div>
+//       <div className="swiper-button-next"></div>
+//     </Swiper>
+//   );
+// }
